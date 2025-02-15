0066dd2b5cc1f3e54b33e6c4a43160191f660039
carrington-115 <frumarkcarrington@gmail.com>
2025-02-15 07:24:32 +0530

    built the calendar widget component

--- ui-components/CalendarWidget.tsx
+++ ui-components/CalendarWidget.tsx
@@ -1,6 +1,34 @@
 import React from "react";
-// import {} from '
 
-export default function componentName() {
-  return <></>;
+interface calendarWidgetProps {
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+  status: "completed" | "pending" | "in-progress";
+}
+
+export default function componentName({
+  title,
+  startDate,
+  endDate,
+  description,
+  status,
+}: calendarWidgetProps) {
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <div>
+          <div />
+          <div>
+            <p>{startDate.toLocaleDateString()}</p>
+            <p>{endDate.toLocaleDateString()}</p>
+          </div>
+        </div>
+        <p>{status}</p>
+      </div>
+    </>
+  );
 }
